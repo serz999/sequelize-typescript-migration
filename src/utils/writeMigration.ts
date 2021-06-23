7496567c8dd90e34b2433f4fc07661398def1e83
Manuel Richarz <manuel.richarz@nysoft.de>
2021-06-24 01:27:01 +0200

    Avoid renameColumn: FOREIGN KEY constraint failed bumped version to 2.0.0-beta.2

--- src/utils/writeMigration.ts
+++ src/utils/writeMigration.ts
@@ -51,13 +51,27 @@ module.exports = {
     {
         var index = this.pos;
         return new Promise(function(resolve, reject) {
-            function next() {
+            async function next() {
                 if (index < migrationCommands.length)
                 {
                     let command = migrationCommands[index];
                     console.log("[#"+index+"] execute: " + command.fn);
                     index++;
-                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    try {
+                      if(command.fn === 'renameColumn') {
+                        await queryInterface.sequelize.query('PRAGMA defer_foreign_keys = ON');
+                        await queryInterface.sequelize.query('PRAGMA foreign_keys = OFF');
+                      }
+                      const result = await queryInterface[command.fn].apply(queryInterface, command.params);
+                      if(command.fn === 'renameColumn') {
+                        await queryInterface.sequelize.query('PRAGMA foreign_keys = ON');
+                        await queryInterface.sequelize.query('PRAGMA defer_foreign_keys = OFF');
+                      }
+                      await next(result);
+                    } catch(e) {
+                      console.error(e);
+                      reject(e);
+                    }
                 }
                 else
                     resolve();
@@ -69,13 +83,27 @@ module.exports = {
     {
         var index = this.pos;
         return new Promise(function(resolve, reject) {
-            function next() {
+            async function next() {
                 if (index < rollbackCommands.length)
                 {
                     let command = rollbackCommands[index];
                     console.log("[#"+index+"] execute: " + command.fn);
                     index++;
-                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+                    try {
+                      if(command.fn === 'renameColumn') {
+                        await queryInterface.sequelize.query('PRAGMA defer_foreign_keys = ON');
+                        await queryInterface.sequelize.query('PRAGMA foreign_keys = OFF');
+                      }
+                      const result = await queryInterface[command.fn].apply(queryInterface, command.params);
+                      if(command.fn === 'renameColumn') {
+                        await queryInterface.sequelize.query('PRAGMA foreign_keys = ON');
+                        await queryInterface.sequelize.query('PRAGMA defer_foreign_keys = OFF');
+                      }
+                      await next(result);
+                    } catch(e) {
+                      console.error(e);
+                      reject(e);
+                    }
                 }
                 else
                     resolve();
