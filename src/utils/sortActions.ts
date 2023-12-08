063642b612ea72746ca82fa75e016123d7f09a90
Сергей Большедворский <serz_999@mail.ru>
2023-12-08 15:29:26 +0300

    ref: if statments

--- src/utils/sortActions.ts
+++ src/utils/sortActions.ts
@@ -21,20 +21,15 @@ export default function sortActions(actions: IAction[]) {
   ];
 
   actions.sort((left: IAction, right: IAction) => {
-    if (
-      orderedActionTypes.indexOf(left.actionType) <
-      orderedActionTypes.indexOf(right.actionType)
-    )
+    if (orderedActionTypes.indexOf(left.actionType) < orderedActionTypes.indexOf(right.actionType)) {
       return -1;
-
-    if (
-      orderedActionTypes.indexOf(left.actionType) >
-      orderedActionTypes.indexOf(right.actionType)
-    )
+    }
+    if (orderedActionTypes.indexOf(left.actionType) > orderedActionTypes.indexOf(right.actionType)) {
       return 1;
-
-    if (left.actionType === "dropTable" && right.actionType === "dropTable")
-      return sortByLengthExists(right.depends, left.depends);
+    }
+    if (left.actionType === "dropTable" && right.actionType === "dropTable") {
+        return sortByLengthExists(right.depends, left.depends);
+    }
 
     return sortByLengthExists(left.depends, right.depends);
   });
@@ -42,22 +37,25 @@ export default function sortActions(actions: IAction[]) {
   for (let i = 0; i < actions.length; i++) {
     const leftAction: IAction = actions[i];
 
-    if (leftAction.depends.length === 0) continue;
+    if (leftAction.depends.length === 0) {
+        continue;
+    }
 
     for (let j = 0; j < actions.length; j++) {
       const rightAction: IAction = actions[j];
 
-      if (rightAction.depends.length === 0) continue;
+      if (rightAction.depends.length === 0 || leftAction.actionType !== rightAction.actionType) {
+        continue;
+      }
 
-      if (leftAction.actionType !== rightAction.actionType) continue;
-
-      if (rightAction.depends.includes(leftAction.tableName))
+      if (rightAction.depends.includes(leftAction.tableName)) {
         if (i > j) {
           const c = actions[i];
 
           actions[i] = actions[j];
           actions[j] = c;
         }
+      } 
     }
   }
 
